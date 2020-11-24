00aca61239bf18802f830ac9790c264030cfb63f
Jeongeun-Choi <cje3080@gmail.com>
2020-11-24 20:20:38 +0900

    feat: [FE] Analyzer 컴포넌트 작성
    - 본문 내용
    ArrayBuffer를 받아서 AudioBuffer로 변환시켜주는 메소드가 작성되어 있다.

---
+++ frontend/src/components/audio-analyzer/index.ts
@@ -0,0 +1,17 @@
+class Analyzer {
+  private audioContext: AudioContext;
+  private audioBuffer: AudioBuffer | null;
+  constructor() {
+    this.audioContext = new AudioContext();
+    this.audioBuffer = null;
+  }
+
+  setAudio(audioFile) {
+    this.audioContext.decodeAudioData(audioFile).then((buffer: AudioBuffer) => {
+      this.audioBuffer = buffer;
+      console.log(this.audioBuffer);
+    });
+  }
+}
+
+export default new Analyzer();
