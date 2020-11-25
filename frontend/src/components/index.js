1408c165cd97e72af80ce04e19f829876cf935ac
Songwonseok <dnjstjr0517@gmail.com>
2020-11-25 13:55:16 +0900

    feat: [FE] Header 컴포넌트 구현 (#45)
    * feat: [FE] Logo 컴포넌트 구현
    
    - README에 있는 AUDI 로고 svg로 변환했음
    - svg 태그로 렌더링, color 속성으로 색상 변경 가능
    
    - #1
    
    * feat: [FE] IconButton 컴포넌트 구현
    
    - iconSet에 모든 icontype 정리
    - color, size, icontype을 속성값으로 줘서 사용
    - blade icontype만 viewBox 값을 다르게 설정
    - 본문 내용
    
     #2
    
    * feat: [FE] EditorMenu 컴포넌트 구현
    
    - IconButton 컴포넌트를 사용하여 구현
    - file, edit, play, user 순으로 tool 모아둠
    - play-tools 같은 경우 아이콘 순서를 audiomass를 참고했음
    - 본문 내용
    
     #3
    
    * feat: [FE] Header 컴포넌트 구현
    
    - Logo, EditorMenu 컴포넌트로 구현
    - 본문 내용
    
    * feat: [FE] icontype record_on 추가
    
    - 녹음 진행중 상태 아이콘 추가
    
    * refactor: [FE] iconSet -> icons로 파일명 변경
    
    - Set이라는 단어가 안맞는것 같아서 iconSet에서 icons로 파일명 변경
    
    * refactor: [FE] scss 정렬
    
    - scss 정렬 도입했음
    -참고: https://webdesign.tutsplus.com/articles/outside-in-ordering-css-properties-by-importance--cms-21685
    
    * style: EOL 추가 및 주석 제거
    
    - 소스파일 맨 마지막에 공백 추가
    - 주석제거
    
    * refactor: [FE] EditorMen 내부 컴포넌트 모듈화
    
    - 따로 state를 관리해야 될 것 같아서 EditTools, PlaybackTools 컴포넌트
      모듈화
    - 모든 버튼에 icontype과 동일한 id값을 속성으로 줬음
    
     #3
    
    * refactor: [FE] PlaybackTools 컴포넌트 구현
    
    - EditorMenu에서 사용되는 재생관련 도구들을 모듈화 시킴
    - 모든 icon-button에는 icontype과 동일한 id값을 줬음
    - 본문 내용
    
    * refactor: [FE] EditTools 컴포넌트 구현
    
    - EditorMenu 컴포넌트에서 사용되는 편집관련 도구들 모듈화
    - 모든 icon-button에 icontype과 동일한 id를 넣었음
    
    * feat: [FE] components index.js에 EditTools, PlaybackTools import
    
    - 따로 모듈화 시킨 EditTools, PlaybackTools 컴포넌트 적용
    
    * style: [FE] scss 정렬방식 변경
    
    - justify-content도 display:flex와 관련있다고 판단하여 같은 그룹으로
      변경
    
    * style: [FE] icontype camelcase 적용
    
    - 스네이크 케이스 -> 카멜케이스로 변경
    
    * style: [FE] index-> 컴포넌트 명으로 변경
    
    - 개발도중 보기 불편해서 컴포넌트 명으로 변경
    - 본문 내용

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,2 +1,9 @@
 import './audio-editor-app';
 import './sample-component';
+import './IconButton';
+import './Logo';
+import './EditorMenu';
+import './Header';
+import './EditTools';
+import './PlaybackTools';
+
