6d6d53ab604ea3509ef35f863c7e4b2f3df15ef4
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-26 03:16:13 +0900

    feat: [FE] 루트 컴포넌트에서 모든 이벤트 처리 구현
    - 루트 컴포넌트에서 모든 이벤트를 처리할 수 있도록 수정

--- frontend/src/components/App/App.ts
+++ frontend/src/components/App/App.ts
@@ -1,26 +1,86 @@
 import "./App.scss";
+import { EventDataType, eventTypes, EventTargetDataType } from "@types";
 
 (() => {
   const App = class extends HTMLElement {
+    private eventListenerCollectors: Map<string, Map<string, EventTargetDataType>> | null;
+    private eventsForListener : string[];
+
     constructor() {
       super();
+      this.eventListenerCollectors = null;
+      this.eventsForListener = eventTypes;
     }
 
-    connectedCallback() {
+    connectedCallback(): void {
       this.render();
+      this.init();
+      this.initEvent();
     }
 
-    render() {
+    render(): void {
       this.innerHTML = `
                   <div class="audi-app-container">
                     <header-component></header-component>
                     <audi-main></audi-main>
+                    <editor-modal type="effect"></editor-modal>
                   </div>
               `;
     }
+
+    init(): void{
+      this.eventListenerCollectors = this.eventsForListener
+        .reduce((acc, cur)=> acc.set(cur, new Map()), new Map());
+    }
+
+    initEvent(): void{
+      this.eventsForListener.forEach((eventName)=>
+        this.addEventListener(eventName, this.eventListenerForRegistrant.bind(this))
+      );
+    }
+
+    eventListenerForRegistrant(e) {
+      const { target } = e;
+      
+      if (!this.isEventTarget(target) || !this.eventListenerCollectors) return;
+      
+      const eventType = e.type;
+      const eventKey = target.getAttribute('event-key');
+      
+      this.excuteEventListenerForTarget(eventType, eventKey, e);
+    }
+
+    isEventTarget(eventTarget: HTMLElement): Boolean {
+      const eventKey = eventTarget.getAttribute('event-key');
+      return (eventKey)? true : false;
+    }
+
+    excuteEventListenerForTarget(eventType: string, eventKey: string, e:Event){
+      if(!this.eventListenerCollectors) return;
+
+      const eventListenerCollector = this.eventListenerCollectors.get(eventType);
+      if(eventListenerCollector){
+        const eventTargetData = eventListenerCollector.get(eventKey);    
+        eventTargetData?.listener.call(eventTargetData.bindObj, e);
+      }
+    }
+
+    registerEventListener(eventData: EventDataType){
+      const { eventTypes, eventKey, listeners, bindObj } = eventData;
+
+      eventTypes.forEach((eventType, idx)=>{
+        if (this.eventListenerCollectors &&this.eventListenerCollectors.has(eventType)){
+          const eventListenerCollector = this.eventListenerCollectors.get(eventType);
+          if(eventListenerCollector){
+            eventListenerCollector.set(eventKey, {listener:listeners[idx],bindObj:bindObj});
+            this.eventListenerCollectors.set(eventType, eventListenerCollector);
+          }
+        }
+      });
+    }
   };
+
   customElements.define('audi-app', App);
 })();
 
-export { };
-
+export {}
