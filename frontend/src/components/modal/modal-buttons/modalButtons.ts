330308b2345c5e57d92b4b6c87c27846e9077cb7
Jeongeun-Choi <cje3080@gmail.com>
2020-11-25 12:06:31 +0900

    feat: [FE] Modal buttons 분리
    - 본문 내용
      - 하드 코딩을 하여 각 모달의 type에 맞게 출력되는 버튼을 만들었습니다.

---
+++ frontend/src/components/modal/modal-buttons/modalButtons.ts
@@ -0,0 +1,6 @@
+export const modalButtons = {
+  source: `
+    <button class='modal-green-button' type='button'>불러오기</button>
+    <button class='modal-close-button' type='button'>취소</button>`,
+  effect: `<button class='modal-close-button' type='button'>취소</button>`
+};
