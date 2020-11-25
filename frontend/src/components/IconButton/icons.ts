01e0f5f82585717cc37f8ef52554990756a9cf16
songwonseok <dnjstjr0517@gmail.com>
2020-11-25 11:00:02 +0900

    refactor: [FE] iconSet -> icons로 파일명 변경
    - Set이라는 단어가 안맞는것 같아서 iconSet에서 icons로 파일명 변경

--- frontend/src/components/IconButton/icons.ts
+++ frontend/src/components/IconButton/icons.ts
@@ -1,4 +1,4 @@
-const iconSet = {
+const icons = {
   play: (color: string) => `<path fillRule="evenodd" fill="${color}" d="M8 5v14l11-7z"></path>`,
   pause: (color: string) => `<path fillRule="evenodd" fill="${color}" d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"></path>`,
   stop: (color: string) => `<path fillRule="evenodd" fill="${color}" d="M6 6h12v12H6z"></path>`,
@@ -33,4 +33,4 @@ const iconSet = {
   link: (color: string) => `<path fillRule="evenodd" fill="${color}" d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"></path>`,
 }
 
-export default iconSet;
+export default icons;
