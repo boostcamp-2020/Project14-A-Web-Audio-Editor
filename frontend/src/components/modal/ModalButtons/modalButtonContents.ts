0422833420f88a11b55a2ab9cf270dee0c39d918
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-26 19:20:02 +0900

    refactor: [FE] ModalButtonContent에 eventKey 등록
    - eventKey 등록

--- frontend/src/components/modal/ModalButtons/modalButtonContents.ts
+++ frontend/src/components/modal/ModalButtons/modalButtonContents.ts
@@ -2,9 +2,9 @@ import { ModalButtonType } from "../modalType/modalType"
 
 const modalButtonContents: ModalButtonType = {
   source: `
-    <button class='modal-green-button' type='button'>불러오기</button>
-    <button class='modal-close-button' type='button'>취소</button>`,
-  effect: `<button class='modal-close-button' type='button'>취소</button>`
+    <button class='modal-green-button' event-key='source-upload' type='button'>불러오기</button>
+    <button class='modal-close-button' event-key='modal-close' type='button'>취소</button>`,
+  effect: `<button class='modal-close-button' event-key='modal-close' type='button'>취소</button>`
 };
 
 export {
