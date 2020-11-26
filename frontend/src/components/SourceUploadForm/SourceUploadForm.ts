3c685d3f9aaa2fc7a068369127bacbe9fcb8f9cf
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-26 19:19:08 +0900

    feat: [FE] SourceUploadForm 수정
    - 이벤트 등록을 루트에서 처리하도록 수정

--- frontend/src/components/SourceUploadForm/SourceUploadForm.ts
+++ frontend/src/components/SourceUploadForm/SourceUploadForm.ts
@@ -1,67 +1,34 @@
 import './SourceUploadForm.scss';
-import Analyzer from '../audio-analyzer';
-
+import { FileUtil, AudioUtil } from '@util';
+import { Source } from "@model";
+import { Controller } from '@controllers';
+import { EventUtil } from "@util";
+import { EventType } from "@types";
+ 
 (() => {
   const SourceUploadForm = class extends HTMLElement {
     private filename: string;
+    private source: Source | null;
     private sourceUploadElement: HTMLElement | null;
+
     constructor() {
       super();
       this.filename = '';
+      this.source = null;
       this.sourceUploadElement = null;
     }
 
     connectedCallback() {
       this.render();
-      this.initDOM();
+      this.initElement();
       this.initEvent();
     }
 
-    initDOM() {
-      this.sourceUploadElement = document.querySelector('.source-upload-content');
-    }
-
-    initEvent() {
-      this.addEventListener('dragover', this.dragOver.bind(this));
-      this.addEventListener('drop', this.uploadFile.bind(this));
-      this.addEventListener('change', this.uploadFile.bind(this));
-    }
-
-    dragOver(e) {
-      e.preventDefault();
-      const { target } = e;
-      if (this.sourceUploadElement && e.type === 'dragover' && target === this.sourceUploadElement) {
-        this.sourceUploadElement.style.background = 'lightgray';
-      }
-    }
-
-    uploadFile(e) {
-      e.preventDefault();
-
-      const ORIGIN_BACKGROUNDCOLOR = '#212121';
-      if (this.sourceUploadElement && e.type === 'drop') {
-        this.sourceUploadElement.style.background = ORIGIN_BACKGROUNDCOLOR;
-      }
-
-      const files = e.target.files || e.dataTransfer.files;
-      const file = files[0];
-
-      if (file) {
-        this.setFilename(file.name);
-        const reader = new FileReader();
-        reader.onload = (e) => Analyzer.setAudio(e?.target.result, this.filename);
-        reader.readAsArrayBuffer(file);
-      }
-    }
-
-    setFilename(filename) {
-      this.filename = filename;
+    reset(){
+      this.filename = '';
+      this.source = null;
       this.render();
-
-      const sourceEmpty: HTMLElement | null = document.querySelector('.source-empty');
-      if (sourceEmpty) {
-        sourceEmpty.className = 'hide-source-upload-content';
-      }
+      this.initElement();
     }
 
     render() {
@@ -75,6 +42,78 @@ import Analyzer from '../audio-analyzer';
                 </label>
             </section>
             `;
+    }
+
+    initElement() {
+      this.sourceUploadElement = document.querySelector('.source-upload-content');
+    }
+
+    initEvent() {
+      this.addEventListener('dragover', this.uploadFormDragOverListener.bind(this));
+      this.addEventListener('drop', this.uploadFileListener.bind(this));
+      this.addEventListener('change', this.uploadFileListener.bind(this));
+
+      EventUtil.registerEventToRoot({
+        eventTypes:[EventType.click], 
+        eventKey: 'source-upload', 
+        listeners:[this.uploadBtnClickListener], 
+        bindObj: this 
+      });
+    }
+
+    uploadBtnClickListener(e){
+      if(!this.source){ 
+        alert('소스를 등록해주세요.');
+        return;
+      };
+
+      //로드중입니다....
+      Controller.addSource(this.source);
+      const modalElement = document.querySelector('editor-modal');
+      modalElement?.hideModal();
+      this.reset();
+    }
+
+    uploadFormDragOverListener(e){
+      e.preventDefault();
+      const { target } = e;
+      if (this.sourceUploadElement && e.type === 'dragover' && target === this.sourceUploadElement) {
+        this.sourceUploadElement.style.background = 'lightgray';
+      }
+    }
+
+    async uploadFileListener(e) {
+      e.preventDefault();
+
+      const ORIGIN_BACKGROUNDCOLOR = '#212121';
+      if (this.sourceUploadElement && e.type === 'drop') {
+        this.sourceUploadElement.style.background = ORIGIN_BACKGROUNDCOLOR;
+      }
+
+      const files = e.target.files || e.dataTransfer.files;
+      const file = files[0];
+
+      if (file) {
+        const { name } = file;
+        this.setFilename(name);
+        await this.setSource(file);
+      }
+    }
+
+    async setSource(file) {
+      const arrayBuffer = await FileUtil.readFileAsync(file);
+      const audioBuffer = await AudioUtil.decodeArrayBufferToAudio(arrayBuffer);
+      this.source = new Source(file, audioBuffer);
+    }
+    
+    setFilename(filename) {
+      this.filename = filename;
+      this.render();
+
+      const sourceEmpty: HTMLElement | null = document.querySelector('.source-empty');
+      if (sourceEmpty) {
+        sourceEmpty.className = 'hide-source-upload-content';
+      }
     }
   };
 
