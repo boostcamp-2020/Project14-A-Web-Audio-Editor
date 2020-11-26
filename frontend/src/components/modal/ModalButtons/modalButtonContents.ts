bb117a7e08e6812feb81d73af2de60b7f92d9f02
songwonseok <dnjstjr0517@gmail.com>
2020-11-26 17:55:06 +0900

    refactor: [FE] modal download 타입 추가
    - 파일 저장하는 modal 컴포넌트를 위한 download타입 추가

--- frontend/src/components/modal/ModalButtons/modalButtonContents.ts
+++ frontend/src/components/modal/ModalButtons/modalButtonContents.ts
@@ -4,7 +4,9 @@ const modalButtonContents: ModalButtonType = {
   source: `
     <button class='modal-green-button' type='button'>불러오기</button>
     <button class='modal-close-button' type='button'>취소</button>`,
-  effect: `<button class='modal-close-button' type='button'>취소</button>`
+  effect: `<button class='modal-close-button' type='button'>취소</button>`,
+  download: `<a class="compress-button" id="download-link" ><button class="save-button" type='button' disabled='disabled'>변환</button></a>
+    <button class='modal-close-button' type='button'>취소</button>`
 };
 
 export {
