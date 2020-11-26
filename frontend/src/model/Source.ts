a5c5672a47a18b368c76158e20bce3aac23352a6
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-26 19:05:05 +0900

    feat: [FE] Source 모델 정의
    - 오디오 파일 소스의 모델정의

--- frontend/src/model/Source.ts
+++ frontend/src/model/Source.ts
@@ -6,15 +6,25 @@ class Source{
     public duration: number;
     public numberOfChannels: number;
     public channelData: Float32Array[];
-     
-    constructor(){
+    public fileSize: number;
+
+    constructor(file: File, audioBuffer: AudioBuffer){
         this.id = 0;
-        this.fileName = '';
-        this.sampleRate = 0;
-        this.length = 0;
-        this.duration = 0;
-        this.numberOfChannels = 0;
+        this.fileName = file.name;
+        this.fileSize = file.size;
+        this.sampleRate = audioBuffer.sampleRate;
+        this.length = audioBuffer.length;
+        this.duration = audioBuffer.duration;
+        this.numberOfChannels = audioBuffer.numberOfChannels;
         this.channelData = [];
+        
+        this.setChannelData(audioBuffer);
+    }
+
+    setChannelData(audioBuffer: AudioBuffer){
+        for(let i = 0; i < this.numberOfChannels; i++){
+            this.channelData[i] = audioBuffer.getChannelData(i);
+        }
     }
 }
 
