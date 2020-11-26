60f2a639aaf584ac1d1e65f3591b59eb97d4497b
songwonseok <dnjstjr0517@gmail.com>
2020-11-26 17:43:58 +0900

    feat: [FE] 파일저장 기능 구현
    - AurrayBuffer, quality(0~1), fileName을 인자로 wav파일을 만들어주는
      기능 구현
      - 저: 0.5 , 중 : 0.75, 고: 1 로 설정
    - 압축이 끝나면 downloadModal창 a태그에 다운로드 링크 생성
    - wav, mp3 파일의 차이점은 음질인것 같음
      - wave 파일 : 무손실 음원, 보통 편집할 때 사용
      - mp3 파일 : 압축시킨 음원, 무손실 음원보단 음질이 덜하지만 용량이
        적음
    
     #22

---
+++ frontend/src/common/util/compressor.ts
@@ -0,0 +1,97 @@
+export const saveFile = async (arrayBuffer: ArrayBuffer, quality: number, fileName: string) => {
+  const audioCtx: AudioContext = new AudioContext();
+  const buffer = await audioCtx.decodeAudioData(arrayBuffer);
+
+  const offlineAudioCtx: OfflineAudioContext = new OfflineAudioContext({
+    numberOfChannels: 2,
+    length: (buffer.sampleRate * quality) * buffer.duration,
+    sampleRate: buffer.sampleRate * quality,
+  });
+
+  const soundSource = offlineAudioCtx.createBufferSource();
+  soundSource.buffer = buffer;
+
+  const compressor = offlineAudioCtx.createDynamicsCompressor();
+
+  compressor.threshold.setValueAtTime(-20, offlineAudioCtx.currentTime);
+  compressor.knee.setValueAtTime(30, offlineAudioCtx.currentTime);
+  compressor.ratio.setValueAtTime(5, offlineAudioCtx.currentTime);
+  compressor.attack.setValueAtTime(.05, offlineAudioCtx.currentTime);
+  compressor.release.setValueAtTime(.25, offlineAudioCtx.currentTime);
+
+  const gainNode = offlineAudioCtx.createGain();
+  gainNode.gain.setValueAtTime(1, offlineAudioCtx.currentTime);
+
+  soundSource.connect(compressor);
+  compressor.connect(gainNode);
+  gainNode.connect(offlineAudioCtx.destination);
+
+  soundSource.start(0);
+  soundSource.loop = false;
+  const renderedBuffer = await offlineAudioCtx.startRendering();
+
+  makeDownload(renderedBuffer, offlineAudioCtx.length, fileName);
+}
+
+
+const makeDownload = (abuffer: AudioBuffer, total_samples: number, fileName: string) => {
+
+  const newFile = URL.createObjectURL(bufferToWave(abuffer, total_samples));
+
+  const downloadLink: HTMLElement | null = document.getElementById("download-link");
+  downloadLink?.setAttribute('href', newFile);
+  downloadLink?.setAttribute('download', fileName);
+
+}
+
+const bufferToWave = (abuffer: AudioBuffer, len: number) => {
+  const numOfChan: number = abuffer.numberOfChannels;
+  const length: number = len * numOfChan * 2 + 44;
+  const buffer: ArrayBuffer = new ArrayBuffer(length);
+  const view: DataView = new DataView(buffer);
+  const channels: Float32Array[] = [];
+  let sample: number = 0;
+  let offset: number = 0;
+  let pos: number = 0;
+
+  const setUint16 = (data) => {
+    view.setUint16(pos, data, true);
+    pos += 2;
+  }
+
+  const setUint32 = (data) => {
+    view.setUint32(pos, data, true);
+    pos += 4;
+  }
+
+  setUint32(0x46464952);
+  setUint32(length - 8);
+  setUint32(0x45564157);
+
+  setUint32(0x20746d66);
+  setUint32(16);
+  setUint16(1);
+  setUint16(numOfChan);
+  setUint32(abuffer.sampleRate);
+  setUint32(abuffer.sampleRate * 2 * numOfChan);
+  setUint16(numOfChan * 2);
+  setUint16(16);
+
+  setUint32(0x61746164);
+  setUint32(length - pos - 4);
+
+  for (let i = 0; i < abuffer.numberOfChannels; i++)
+    channels.push(abuffer.getChannelData(i));
+
+  while (pos < length) {
+    for (let i = 0; i < numOfChan; i++) {
+      sample = Math.max(-1, Math.min(1, channels[i][offset]));
+      sample = (0.5 + sample < 0 ? sample * 32768 : sample * 32767) | 0;
+      view.setInt16(pos, sample, true);
+      pos += 2;
+    }
+    offset++
+  }
+
+  return new Blob([buffer], { type: "audio/wav" });
+}
