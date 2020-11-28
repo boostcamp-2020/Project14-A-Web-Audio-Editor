279b7609457d484009106192af7538ee6051fbb3
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-29 07:04:31 +0900

    refactor: [FE] EventType 수정
    - 파일 관리를 위해 Event 관련 타입들을 모두 eventTypes.ts 파일로 이동
    - 이벤트키 관리를 위해 EventKeyType 정의

---
+++ frontend/src/common/types/eventTypes.ts
@@ -0,0 +1,38 @@
+enum EventKeyType {
+    SOURCE_UPLOAD_LOAD_BTN_CLICK = 'SOURCE_UPLOAD_LOAD_BTN_CLICK',
+    SOURCE_UPLOAD_CLOSE_CLOSE_BTN_CLICK = 'SOURCE_UPLOAD_CLOSE_CLOSE_BTN_CLICK',
+    SOURCE_UPLOAD_CONTENT_MULTIPLE = 'SOURCE_UPLOAD_CONTENT_MULTIPLE',
+    SOURCE_DOWNLOAD_FILE_NAME_KEYUP = 'SOURCE_DOWNLOAD_FILE_NAME_KEYUP',
+    SOURCE_DOWNLOAD_SAVE_BTN_CLICK = 'SOURCE_DOWNLOAD_SAVE_BTN_CLICK',
+    SOURCE_DOWNLOAD_CLOSE_BTN_CLICK = 'SOURCE_DOWNLOAD_CLOSE_BTN_CLICK'
+}
+
+enum EventType {
+    click = 'click',
+    keyup = 'keyup',
+    dragover = 'dragover',
+    drop = 'drop',
+    change = 'change'
+}
+
+const eventTypes = ['click', 'keyup', 'dragover', 'drop', 'change'];
+
+interface EventTargetDataType{
+    listener: EventListener;
+    bindObj: Object;
+}
+
+interface EventDataType{
+    eventTypes: EventType[];
+    eventKey: string;
+    listeners: EventListener[];
+    bindObj: Object;
+}
+
+export {
+    EventKeyType,
+    EventType,
+    eventTypes,
+    EventTargetDataType,
+    EventDataType
+}
