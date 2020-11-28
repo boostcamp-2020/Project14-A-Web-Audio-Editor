9efa887cf98752226d0b88e9fc23ff8d8c15e0ed
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-29 07:27:41 +0900

    feat: [FE] Modal 관련 state 처리 추가
    - Modal 상태 관리 위해 store에 Modal state를 추가
    - Modal 상태 변경을 위해 Controller에 상태 변경 작업 추가

--- frontend/src/controllers/controller.ts
+++ frontend/src/controllers/controller.ts
@@ -1,10 +1,16 @@
 import { Source } from '@model';
 import { store } from "@store";
+import { ModalType } from "@types";
 
 const addSource = (source: Source): void =>{
     store.setSource(source);
 }
 
+const changeModalState = (modalType: ModalType, isHidden: Boolean): void => {
+    store.setModalState(modalType, isHidden);
+}
+
 export default {
-    addSource
+    addSource,
+    changeModalState
 }
