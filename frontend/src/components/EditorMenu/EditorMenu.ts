7d7d76f82a126275de96fdfca08be3f34720b687
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-29 10:07:29 +0900

    refactor: [FE] EditorMenu 컴포넌트 이벤트 처리 방식 변경
    - 루트에서 이벤트를 처리하도록 코드 수정

--- frontend/src/components/EditorMenu/EditorMenu.ts
+++ frontend/src/components/EditorMenu/EditorMenu.ts
@@ -1,67 +1,61 @@
 import './EditorMenu.scss';
-
+import { Controller } from '@controllers';
+import { ModalType, EventType, EventKeyType } from '@types';
+import { EventUtil } from '@util';
+ 
 (() => {
   const EditorMenu = class extends HTMLElement {
     constructor() {
       super();
     }
 
-    connectedCallback() {
+    connectedCallback(): void {
       this.render();
       this.initEvent();
     }
 
-    initEvent() {
-      this.addEventListener('click', this.openSourceUploadForm.bind(this));
-      this.addEventListener('click', this.openSourceDownloadForm.bind(this));
-    }
-
-    openSourceUploadForm(e) {
-      const { target } = e;
-      const targetElement = target.closest('#upload');
-      const uploadElement = document.getElementById('upload');
-
-      if (targetElement === uploadElement) {
-        const uploadModalElement = document.getElementById('source');
-        const modalElement = uploadModalElement?.closest('audi-modal');
-
-        modalElement?.showModal();
-      }
-    }
-
-    openSourceDownloadForm(e) {
-      const { target } = e;
-      const targetElement = target.closest('#save');
-      if (!targetElement) return;
-      const downloadModal = document.getElementById('save');
-
-      if (targetElement === downloadModal) {
-        const downloadModalElement = document.getElementById('download');
-        const modalElement = downloadModalElement?.closest('audi-modal');
-
-        modalElement?.showModal();
-      }
-    }
-
-    render() {
+    render(): void {
       this.innerHTML = `
               <div id="editor-menu">
                 <div class="icon-wrap">
-                  <audi-icon-button id="upload" color="white" icontype="upload" size="32px"></audi-icon-button>
-                  <audi-icon-button id="save" color="white" icontype="save" size="32px"></audi-icon-button>
+                  <audi-icon-button id="upload" color="white" icontype="upload" size="32px" data-event-key=${EventKeyType.EDITOR_MENU_OPEN_UPLOAD_BTN_CLICK}></audi-icon-button>
+                  <audi-icon-button id="save" color="white" icontype="save" size="32px" data-event-key=${EventKeyType.EDITOR_MENU_OPEN_DOWNLOAD_BTN_CLICK}></audi-icon-button>
                 </div>
                 <audi-edit-tools></audi-edit-tools>
                 <audi-playback-tools></audi-playback-tools>
                 <div class="icon-wrap">
                   <audi-icon-button id="record" color="white" icontype="record" size="32px"></audi-icon-button>
                 </div>
-
                 <div id="user-menu" class="icon-wrap">
                   <audi-icon-button color="white" icontype="user" size="32px"></audi-icon-button>
                   <audi-icon-button color="white" icontype="menu" size="32px"></audi-icon-button>
                 </div>
               </div>
             `;
+    }
+
+    initEvent(): void {
+      EventUtil.registerEventToRoot({
+        eventTypes: [EventType.click],
+        eventKey: EventKeyType.EDITOR_MENU_OPEN_UPLOAD_BTN_CLICK,
+        listeners: [this.openUploadModalBtnClickListener],
+        bindObj: this
+      });
+
+      EventUtil.registerEventToRoot({
+        eventTypes: [EventType.click],
+        eventKey: EventKeyType.EDITOR_MENU_OPEN_DOWNLOAD_BTN_CLICK,
+        listeners: [this.openDownloadModalBtnClickListener],
+        bindObj: this
+      });
+    }
+
+    openUploadModalBtnClickListener(): void{
+      Controller.changeModalState(ModalType.upload, false);
+    }
+
+    openDownloadModalBtnClickListener(): void{
+      Controller.changeModalState(ModalType.download, false);
     }
   };
 
