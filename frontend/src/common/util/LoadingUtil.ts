e066db34484ebacdbf2c1ae9acd6c74048bb6284
Jeongeun-Choi <cje3080@gmail.com>
2020-12-01 11:23:29 +0900

    refactor: [FE]LoadingUtil 삭제
    Loading 클래스에 해당 로직을 추가하여 삭제했습니다.

--- frontend/src/common/util/LoadingUtil.ts
+++
@@ -1,9 +0,0 @@
-const startLoading = (element: HTMLElement): void => {
-  element.classList.remove('hide');
-};
-
-const endLoading = (element: HTMLElement): void => {
-  element.classList.add('hide');
-};
-
-export { startLoading, endLoading };
--- frontend/src/common/util/index.js
+++ frontend/src/common/util/index.js
@@ -1,5 +1,4 @@
 export * as EventUtil from './EventUtil';
 export * as FileUtil from './FileUtil';
 export * as AudioUtil from './AudioUtil';
-export * as LoadingUtil from './LoadingUtil';
 export { saveFile } from './compressor';
