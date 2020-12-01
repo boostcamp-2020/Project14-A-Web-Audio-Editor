13117bfe02e0e9f96821f35e5e3c810eb67a6f28
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-12-02 04:53:48 +0900

    feat: [FE] Track, TrackSection 모델 정의
    - Store에 유지할 Track 및 TrackSection 모델 정

--- frontend/src/model/index.js
+++ frontend/src/model/index.js
@@ -1,1 +1,3 @@
-export {default as Source} from "./Source";
+export {default as Source} from './Source';
+export {default as Track} from './Track';
+export {default as TrackSection} from './TrackSection'
