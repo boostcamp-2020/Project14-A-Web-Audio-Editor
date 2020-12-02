88ce928d8f20febe6fe3a1d3acb08338909f3ebf
Jeongeun-Choi <cje3080@gmail.com>
2020-12-02 16:08:44 +0900

    feat: [FE]storetype 추가
    - store의 cursorTime state가 추가되어 해당 타입도 같이 추가했습니다.

--- frontend/src/common/types/storeTypes.ts
+++ frontend/src/common/types/storeTypes.ts
@@ -1,23 +1,21 @@
-import { Source } from "@model"
-import { ModalStateType } from "@types"
+import { Source } from '@model';
+import { ModalStateType } from '@types';
 
 enum StoreChannelType {
-    SOURCE_LIST_CHANNEL = 'SOURCE_LIST_CHANNEL',
-    MODAL_STATE_CHANNEL = 'MODAL_STATE_CHANNEL'
+  SOURCE_LIST_CHANNEL = 'SOURCE_LIST_CHANNEL',
+  MODAL_STATE_CHANNEL = 'MODAL_STATE_CHANNEL',
+  CURSOR_TIME_CHANNEL = 'CURSOR_TIME_CHANNEL'
 }
 
 interface StoreStateType {
-    sourceList: Source[];
-    modalState: ModalStateType;
+  cursorTime: string;
+  sourceList: Source[];
+  modalState: ModalStateType;
 }
 
 interface StoreObserverData {
-    callback: Function;
-    bindObj: Object;
+  callback: Function;
+  bindObj: Object;
 }
 
-export{
-    StoreStateType,
-    StoreChannelType,
-    StoreObserverData
-}
+export { StoreStateType, StoreChannelType, StoreObserverData };
