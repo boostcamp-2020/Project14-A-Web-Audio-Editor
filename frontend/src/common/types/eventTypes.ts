23bbe1ae483ef721eb1ce2baac3ec75f28e1eb30
Jeongeun-Choi <cje3080@gmail.com>
2020-12-02 16:10:24 +0900

    feat: [FE]eventType 추가
    - dbleclick 이벤트 타입을 추가했습니다.
    - mousemove 이벤트 타입을 추가했습니다.
    - PLAYBAR_MULTIPLE 이벤트 키 타입을 추가했습니다.

--- frontend/src/common/types/eventTypes.ts
+++ frontend/src/common/types/eventTypes.ts
@@ -1,42 +1,39 @@
 enum EventKeyType {
-    SOURCE_UPLOAD_LOAD_BTN_CLICK = 'SOURCE_UPLOAD_LOAD_BTN_CLICK',
-    SOURCE_UPLOAD_CLOSE_CLOSE_BTN_CLICK = 'SOURCE_UPLOAD_CLOSE_CLOSE_BTN_CLICK',
-    SOURCE_UPLOAD_CONTENT_MULTIPLE = 'SOURCE_UPLOAD_CONTENT_MULTIPLE',
-    SOURCE_DOWNLOAD_FILE_NAME_KEYUP = 'SOURCE_DOWNLOAD_FILE_NAME_KEYUP',
-    SOURCE_DOWNLOAD_SAVE_BTN_CLICK = 'SOURCE_DOWNLOAD_SAVE_BTN_CLICK',
-    SOURCE_DOWNLOAD_CLOSE_BTN_CLICK = 'SOURCE_DOWNLOAD_CLOSE_BTN_CLICK',
-    SOURCE_DOWNLOAD_EXTENTION_CHANGE = 'SOURCE_DOWNLOAD_EXTENTION_CHANGE',
-    EDITOR_MENU_OPEN_UPLOAD_BTN_CLICK = 'EDITOR_MENU_OPEN_UPLOAD_BTN_CLICK',
-    EDITOR_MENU_OPEN_DOWNLOAD_BTN_CLICK = 'EDITOR_MENU_OPEN_DOWNLOAD_BTN_CLICK',
-    EFFECT_LIST_CLOSE_BTN_CLICK = 'EFFECT_LIST_CLOSE_BTN_CLICK'
+  SOURCE_UPLOAD_LOAD_BTN_CLICK = 'SOURCE_UPLOAD_LOAD_BTN_CLICK',
+  SOURCE_UPLOAD_CLOSE_CLOSE_BTN_CLICK = 'SOURCE_UPLOAD_CLOSE_CLOSE_BTN_CLICK',
+  SOURCE_UPLOAD_CONTENT_MULTIPLE = 'SOURCE_UPLOAD_CONTENT_MULTIPLE',
+  SOURCE_DOWNLOAD_FILE_NAME_KEYUP = 'SOURCE_DOWNLOAD_FILE_NAME_KEYUP',
+  SOURCE_DOWNLOAD_SAVE_BTN_CLICK = 'SOURCE_DOWNLOAD_SAVE_BTN_CLICK',
+  SOURCE_DOWNLOAD_CLOSE_BTN_CLICK = 'SOURCE_DOWNLOAD_CLOSE_BTN_CLICK',
+  SOURCE_DOWNLOAD_EXTENTION_CHANGE = 'SOURCE_DOWNLOAD_EXTENTION_CHANGE',
+  EDITOR_MENU_OPEN_UPLOAD_BTN_CLICK = 'EDITOR_MENU_OPEN_UPLOAD_BTN_CLICK',
+  EDITOR_MENU_OPEN_DOWNLOAD_BTN_CLICK = 'EDITOR_MENU_OPEN_DOWNLOAD_BTN_CLICK',
+  EFFECT_LIST_CLOSE_BTN_CLICK = 'EFFECT_LIST_CLOSE_BTN_CLICK',
+  PLAYBAR_MULTIPLE = 'PLAYBAR_MULTIPLE'
 }
 
 enum EventType {
-    click = 'click',
-    keyup = 'keyup',
-    dragover = 'dragover',
-    drop = 'drop',
-    change = 'change'
+  click = 'click',
+  dblclick = 'dblclick',
+  keyup = 'keyup',
+  dragover = 'dragover',
+  drop = 'drop',
+  change = 'change',
+  mousemove = 'mousemove'
 }
 
-const eventTypes = ['click', 'keyup', 'dragover', 'drop', 'change'];
+const eventTypes = ['click', 'dblclick', 'keyup', 'dragover', 'drop', 'change', 'mousemove'];
 
 interface EventTargetDataType {
-    listener: EventListener;
-    bindObj: Object;
+  listener: EventListener;
+  bindObj: Object;
 }
 
 interface EventDataType {
-    eventTypes: EventType[];
-    eventKey: string;
-    listeners: EventListener[];
-    bindObj: Object;
+  eventTypes: EventType[];
+  eventKey: string;
+  listeners: EventListener[];
+  bindObj: Object;
 }
 
-export {
-    EventKeyType,
-    EventType,
-    eventTypes,
-    EventTargetDataType,
-    EventDataType
-}
+export { EventKeyType, EventType, eventTypes, EventTargetDataType, EventDataType };
