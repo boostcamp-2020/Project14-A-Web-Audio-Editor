8b351a1a7f2bccb21999f14bff3084959218838e
songwonseok <dnjstjr0517@gmail.com>
2020-12-02 21:23:52 +0900

    feat: [FE] store에 ctrlIsPressed 추가
    - ctrl 키가 눌렸는지 확인할 수 있는 ctrlIsPressed 변수 추가

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,55 +1,58 @@
-import { StoreStateType } from "@types";
+import { StoreStateType, CursorType } from "@types";
 import { Track, Source, TrackSection } from "@model";
 import { StoreChannelType, ModalType, ModalStateType } from "@types";
 import { storeChannel } from "@store";
 
-const store = new (class Store{
+const store = new (class Store {
     private state: StoreStateType;
 
-    constructor(){
+    constructor() {
         this.state = {
             cursorTime: '00:00:000',
-            sourceList:[],
+            sourceList: [],
             modalState: {
                 modalType: ModalType.upload,
                 isHidden: true
             },
             isTrackDraggable: false,
-            trackList: this.initTrackList(3)
+            trackList: this.initTrackList(3),
+            focusList: [],
+            ctrlIsPressed: false,
+            cursorMode: CursorType.SELECT_MODE,
         }
     }
 
-    initTrackList(numOfTracks: number): Track[]{
+    initTrackList(numOfTracks: number): Track[] {
         return Array(numOfTracks).fill(0).reduce((acc, cur, idx) => {
-              const track = new Track({id:idx, trackSectionList: []});
-              return acc.concat(track);
-          },[]);
-      }
+            const track = new Track({ id: idx, trackSectionList: [] });
+            return acc.concat(track);
+        }, []);
+    }
 
     getState(): StoreStateType {
         return this.state;
     }
 
-    setSource(source: Source): void{
+    setSource(source: Source): void {
         const { sourceList } = this.state;
         source.id = sourceList.length;
 
         const newSourceList = sourceList.concat(source);
-        this.state = {...this.state, sourceList: newSourceList};
+        this.state = { ...this.state, sourceList: newSourceList };
         storeChannel.publish(StoreChannelType.SOURCE_LIST_CHANNEL, newSourceList);
     }
 
-    setModalState(newModalType: ModalType, newIsHiiden: Boolean): void{
+    setModalState(newModalType: ModalType, newIsHiiden: Boolean): void {
         const { modalState } = this.state;
         const { modalType, isHidden } = modalState;
-        
-        if(modalType === newModalType && isHidden === newIsHiiden) return;
 
-        const newModalState: ModalStateType = {modalType: newModalType, isHidden: newIsHiiden};
-        this.state = {...this.state, modalState: newModalState };
+        if (modalType === newModalType && isHidden === newIsHiiden) return;
+
+        const newModalState: ModalStateType = { modalType: newModalType, isHidden: newIsHiiden };
+        this.state = { ...this.state, modalState: newModalState };
         storeChannel.publish(StoreChannelType.MODAL_STATE_CHANNEL, newModalState);
     }
-    
+
     setCursorTime(newMinute: string, newSecond: string, newMilsecond): void {
         const { cursorTime } = this.state;
         const [minute, second, milsecond] = cursorTime.split(':');
@@ -61,45 +64,49 @@ const store = new (class Store{
         storeChannel.publish(StoreChannelType.CURSOR_TIME_CHANNEL, newCursorTime);
     }
 
-    setTrackDragState(newIsTrackDraggable: Boolean): void{
+    setTrackDragState(newIsTrackDraggable: Boolean): void {
         const { isTrackDraggable } = this.state;
-        if(isTrackDraggable === newIsTrackDraggable) return;
+        if (isTrackDraggable === newIsTrackDraggable) return;
 
         this.state = { ...this.state, isTrackDraggable: newIsTrackDraggable };
         storeChannel.publish(StoreChannelType.TRACK_DRAG_STATE_CHANNEL, newIsTrackDraggable);
     }
 
-    setTrack(newTrack: Track): void{
+    setTrack(newTrack: Track): void {
         const { trackList } = this.state;
 
         newTrack.id = trackList.length;
         const newAudioTrackList = trackList.concat(newTrack);
-        
-        this.state = {...this.state, trackList: newAudioTrackList};
+
+        this.state = { ...this.state, trackList: newAudioTrackList };
         console.log(this.state);
     }
 
-    setTrackSection(trackId: number, newTrackSection: TrackSection): void{
+    setTrackSection(trackId: number, newTrackSection: TrackSection): void {
         const { trackList } = this.state;
-        if(trackId < 0 || trackId >= trackList.length) return;
+        if (trackId < 0 || trackId >= trackList.length) return;
 
         const track = trackList[trackId];
         const { trackSectionList } = track;
         newTrackSection.id = trackSectionList.length;
 
         const newTrackSectionList = trackSectionList
-            .concat(newTrackSection).sort((a, b)=> a.trackStartTime - b.trackStartTime);
+            .concat(newTrackSection).sort((a, b) => a.trackStartTime - b.trackStartTime);
 
-        const newTrack = new Track({...track, trackSectionList: newTrackSectionList});
-        const newTrackList: Array<Track> = trackList.reduce<Track[]>((acc, track)=>
-            (track.id === trackId)? acc.concat(newTrack) : acc.concat(track), 
-        []);
+        const newTrack = new Track({ ...track, trackSectionList: newTrackSectionList });
+        const newTrackList: Array<Track> = trackList.reduce<Track[]>((acc, track) =>
+            (track.id === trackId) ? acc.concat(newTrack) : acc.concat(track),
+            []);
 
-        this.state = {...this.state, trackList: newTrackList};
+        this.state = { ...this.state, trackList: newTrackList };
         storeChannel.publish(StoreChannelType.TRACK_SECTION_LIST_CHANNEL, {
-            trackId:trackId,
+            trackId: trackId,
             trackSectionList: newTrackSectionList
         });
+    }
+
+    setCtrlIsPressed(isPressed: boolean): void {
+        this.state.ctrlIsPressed = isPressed;
     }
 })();
 
