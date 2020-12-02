c3d5c93c956b919634a0f9587d9aa7a48e8b5eb3
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-12-02 10:54:19 +0900

    feat: [FE] StoreChannelType 추가
    - TRACK의 드랍존을 활성화하기 위한 트랙 드래그 상태정보 채널 정의
    - 트랙의 섹션 리스트를 관리하기 위한 트랙 섹션 리스트 상태정보 채널 정의

--- frontend/src/common/types/storeTypes.ts
+++ frontend/src/common/types/storeTypes.ts
@@ -1,14 +1,18 @@
-import { Source } from "@model"
+import { Source, Track } from "@model"
 import { ModalStateType } from "@types"
 
 enum StoreChannelType {
     SOURCE_LIST_CHANNEL = 'SOURCE_LIST_CHANNEL',
-    MODAL_STATE_CHANNEL = 'MODAL_STATE_CHANNEL'
+    MODAL_STATE_CHANNEL = 'MODAL_STATE_CHANNEL',
+    TRACK_DRAG_STATE_CHANNEL = 'TRACK_DRAG_STATE_CHANNEL',
+    TRACK_SECTION_LIST_CHANNEL = 'TRACK_SECTION_LIST_CHANNEL'
 }
 
 interface StoreStateType {
     sourceList: Source[];
     modalState: ModalStateType;
+    isTrackDraggable: Boolean;
+    trackList : Track[];
 }
 
 interface StoreObserverData {
