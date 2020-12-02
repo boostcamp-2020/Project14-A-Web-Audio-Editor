076b3e41362e334d477b45727a1e2a1bee0ac52b
Jeongeun-Choi <cje3080@gmail.com>
2020-12-02 16:07:25 +0900

    feat: [FE]changeCursorTime 함수 추가
    - cursorTime이 변화할 때 사용하는 changeCursorTime 함수를 구현했습니다.

--- frontend/src/controllers/controller.ts
+++ frontend/src/controllers/controller.ts
@@ -1,16 +1,21 @@
 import { Source } from '@model';
-import { store } from "@store";
-import { ModalType } from "@types";
+import { store } from '@store';
+import { ModalType } from '@types';
 
-const addSource = (source: Source): void =>{
-    store.setSource(source);
-}
+const addSource = (source: Source): void => {
+  store.setSource(source);
+};
 
 const changeModalState = (modalType: ModalType, isHidden: Boolean): void => {
-    store.setModalState(modalType, isHidden);
-}
+  store.setModalState(modalType, isHidden);
+};
+
+const changeCursorTime = (minute: string, second: string, milsecond: string): void => {
+  store.setCursorTime(minute, second, milsecond);
+};
 
 export default {
-    addSource,
-    changeModalState
-}
+  addSource,
+  changeModalState,
+  changeCursorTime
+};
