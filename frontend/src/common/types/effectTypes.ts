0894e1577e8e8a015a1a3985b925cb2ad49bcaa6
songwonseok <dnjstjr0517@gmail.com>
2020-12-02 16:02:30 +0900

    feat: [FE] GainEffect 컴포넌트 구현
    - Gain 이벤트타입에 대한 컴포넌트 구현
    - data-percentage로 현재 gain 이펙트에 적용되는 percentage를 속성으로
      전달
      - effectTypes에 일단 초기값 100을 넣어주도록 했음
    - 해당 오디오구간에 effect 설정값을 어떤식으로 넘겨주냐에 따라
        리팩토링 필요
    
     #8

--- frontend/src/common/types/effectTypes.ts
+++ frontend/src/common/types/effectTypes.ts
@@ -7,7 +7,7 @@ enum EffectTitleType {
 }
 
 enum EffectContentType {
-  gain = '<audi-effect-gain></audi-effect-gain>',
+  gain = '<audi-effect-gain data-percentage="100"></audi-effect-gain>',
 }
 
 interface EffectStateType {
