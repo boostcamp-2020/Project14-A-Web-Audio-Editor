7d0fd81eb102f70f64f188ecb86b9695b3acf8f3
Jeongeun-Choi <cje3080@gmail.com>
2020-12-02 16:05:19 +0900

    feat: [FE]store state 추가
    - cursorTime 이라는 state를 추가했습니다.
    - mouseover 이벤트시 바뀌는 cursorTime을 실시간으로 바뀌기 위해 setCursorTime이라는 함수를 추가했습니다.

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,42 +1,52 @@
-import { StoreStateType } from "@types";
-import { Source } from "../model"
-import { StoreChannelType, ModalType, ModalStateType } from "@types"
-import { storeChannel } from "@store";
+import { StoreStateType } from '@types';
+import { Source } from '../model';
+import { StoreChannelType, ModalType, ModalStateType } from '@types';
+import { storeChannel } from '@store';
 
-const store = new (class Store{
-    private state: StoreStateType;
+const store = new (class Store {
+  private state: StoreStateType;
 
-    constructor(){
-        this.state = {
-            sourceList:[],
-            modalState: {
-                modalType: ModalType.upload,
-                isHidden: true
-            }
-        }
-    }
+  constructor() {
+    this.state = {
+      cursorTime: '00:00:000',
+      sourceList: [],
+      modalState: {
+        modalType: ModalType.upload,
+        isHidden: true
+      }
+    };
+  }
 
-    setSource(source: Source): void{
-        const { sourceList } = this.state;
-        source.id = sourceList.length;
+  setSource(source: Source): void {
+    const { sourceList } = this.state;
+    source.id = sourceList.length;
 
-        const newSourceList = sourceList.concat(source);
-        this.state = {...this.state, sourceList: newSourceList};
-        storeChannel.publish(StoreChannelType.SOURCE_LIST_CHANNEL, newSourceList);
-    }
+    const newSourceList = sourceList.concat(source);
+    this.state = { ...this.state, sourceList: newSourceList };
+    storeChannel.publish(StoreChannelType.SOURCE_LIST_CHANNEL, newSourceList);
+  }
 
-    setModalState(newModalType: ModalType, newIsHiiden: Boolean): void{
-        const { modalState } = this.state;
-        const { modalType, isHidden } = modalState;
-        
-        if(modalType === newModalType && isHidden === newIsHiiden) return;
+  setModalState(newModalType: ModalType, newIsHiiden: Boolean): void {
+    const { modalState } = this.state;
+    const { modalType, isHidden } = modalState;
 
-        const newModalState: ModalStateType = {modalType: newModalType, isHidden: newIsHiiden};
-        this.state = {...this.state, modalState: newModalState };
-        storeChannel.publish(StoreChannelType.MODAL_STATE_CHANNEL, newModalState);
-    }
+    if (modalType === newModalType && isHidden === newIsHiiden) return;
+
+    const newModalState: ModalStateType = { modalType: newModalType, isHidden: newIsHiiden };
+    this.state = { ...this.state, modalState: newModalState };
+    storeChannel.publish(StoreChannelType.MODAL_STATE_CHANNEL, newModalState);
+  }
+
+  setCursorTime(newMinute: string, newSecond: string, newMilsecond): void {
+    const { cursorTime } = this.state;
+    const [minute, second, milsecond] = cursorTime.split(':');
+
+    if (minute === newMinute && second === newSecond && milsecond === newMilsecond) return;
+
+    const newCursorTime: string = `${newMinute.padStart(2, '0')}:${newSecond.padStart(2, '0')}:${newMilsecond.padStart(3, '0')}`;
+    this.state = { ...this.state, cursorTime: newCursorTime };
+    storeChannel.publish(StoreChannelType.CURSOR_TIME_CHANNEL, newCursorTime);
+  }
 })();
 
-export {
-    store
-}
+export { store };
