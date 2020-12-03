6e7f6ac624a3483e18554502ea6c782c8b307f8b
Jeongeun-Choi <cje3080@gmail.com>
2020-12-03 11:52:42 +0900

    feat: [FE] currentPosition 관련 함수 생성
    - getCurrentPosition: currentPosition을 반환하는 함수
    - changeCurrentPosition: currentPosition을 변환시켜주는 함수

--- frontend/src/controllers/controller.ts
+++ frontend/src/controllers/controller.ts
@@ -1,43 +1,43 @@
 import { Source, Track, TrackSection } from '@model';
-import { store } from "@store";
-import { ModalType } from "@types";
+import { store } from '@store';
+import { ModalType } from '@types';
 
-const getSectionChannelData = (trackId: number, trackSectionId: number): number[] | undefined => { 
-    const { trackList, sourceList } = store.getState();
-    const track = trackList.find((track) => (track.id === trackId));
-    // console.log(track);
-    if(!track) return;
-    
-    const { trackSectionList } = track;
-    const trackSection = trackSectionList.find((trackSection) => (trackSection.id === trackSectionId));
-    // console.log(trackSection);
-    if(!trackSection) return;
+const getSectionChannelData = (trackId: number, trackSectionId: number): number[] | undefined => {
+  const { trackList, sourceList } = store.getState();
+  const track = trackList.find((track) => track.id === trackId);
+  // console.log(track);
+  if (!track) return;
 
-    const source = sourceList.find((source) => (source.id === trackSection.sourceId));
-    // console.log(source);
-    if(!source) return;
+  const { trackSectionList } = track;
+  const trackSection = trackSectionList.find((trackSection) => trackSection.id === trackSectionId);
+  // console.log(trackSection);
+  if (!trackSection) return;
 
-    const { channelData, parsedChannelData, duration, length, sampleRate } = source;
-    const { parsedChannelStartTime, parsedChannelEndTime } = trackSection;
-    console.log("channelData", channelData);
-    console.log("parsedChannelData", parsedChannelData);
-    console.log("duration", duration);
-    console.log("length", length);
-    console.log("sampleRate", sampleRate);
+  const source = sourceList.find((source) => source.id === trackSection.sourceId);
+  // console.log(source);
+  if (!source) return;
 
-    return parsedChannelData;
-}
+  const { channelData, parsedChannelData, duration, length, sampleRate } = source;
+  const { parsedChannelStartTime, parsedChannelEndTime } = trackSection;
+  console.log('channelData', channelData);
+  console.log('parsedChannelData', parsedChannelData);
+  console.log('duration', duration);
+  console.log('length', length);
+  console.log('sampleRate', sampleRate);
+
+  return parsedChannelData;
+};
 
 const getSourceBySourceId = (sourceId: number): Source | undefined => {
-    const { sourceList } = store.getState();
-    const source = sourceList.find((source) => (source.id === sourceId));
+  const { sourceList } = store.getState();
+  const source = sourceList.find((source) => source.id === sourceId);
 
-    return source;
-}
+  return source;
+};
 
-const addSource = (source: Source): void =>{
-    store.setSource(source);
-}
+const addSource = (source: Source): void => {
+  store.setSource(source);
+};
 
 const changeModalState = (modalType: ModalType, isHidden: Boolean): void => {
   store.setModalState(modalType, isHidden);
@@ -47,31 +47,42 @@ const changeCursorTime = (minute: string, second: string, milsecond: string): vo
   store.setCursorTime(minute, second, milsecond);
 };
 
-const changeTrackDragState = (isTrackDraggable: Boolean): void =>{
-    store.setTrackDragState(isTrackDraggable);
-}
+const changeTrackDragState = (isTrackDraggable: Boolean): void => {
+  store.setTrackDragState(isTrackDraggable);
+};
 
-const getTrackList = (): Track[] =>{
-    const { trackList } = store.getState();
-    return trackList;
-}
+const getTrackList = (): Track[] => {
+  const { trackList } = store.getState();
+  return trackList;
+};
 
 const addTrack = (track: Track): void => {
-    store.setTrack(track);
-}
+  store.setTrack(track);
+};
 
 const addTrackSection = (trackId: number, trackSection: TrackSection): void => {
-    store.setTrackSection(trackId, trackSection);
-}
+  store.setTrackSection(trackId, trackSection);
+};
+
+const changeCurrentPosition = (currentPosition: number): void => {
+  store.setCurrentPosition(currentPosition);
+};
+
+const getCurrentPosition = (): number => {
+  const { currentPosition } = store.getState();
+  return currentPosition;
+};
 
 export default {
-    getSourceBySourceId,
-    getSectionChannelData,
-    addSource,
-    changeModalState,
-    changeTrackDragState,
-    getTrackList,
-    addTrack,
-    addTrackSection,
-    changeCursorTime
-}
+  getSourceBySourceId,
+  getSectionChannelData,
+  addSource,
+  changeModalState,
+  changeTrackDragState,
+  getTrackList,
+  addTrack,
+  addTrackSection,
+  changeCursorTime,
+  changeCurrentPosition,
+  getCurrentPosition
+};
