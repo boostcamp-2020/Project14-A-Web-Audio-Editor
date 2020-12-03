07670759c9c8cab032fbf1a169aab9e03e0fe3d7
songwonseok <dnjstjr0517@gmail.com>
2020-12-03 11:55:17 +0900

    feat: [FE] 구간선택 초기화 이벤트를 위한 범위 설정
    - ctrl키를 누르지 않은 상태에서 audi-main-audio-track-container 영역을
      클릭하면 기존에 선택된 focus가 초기화됩니다.
    - Main과 AudioTrack 둘다 이벤트를 적용했을 때 부자연스럽지 않아서 두 곳
      모두 이벤트를 적용했습니다.

--- frontend/src/components/AudioTrack/AudioTrack.ts
+++ frontend/src/components/AudioTrack/AudioTrack.ts
@@ -6,147 +6,163 @@ import { TrackSection } from "@model";
 import "./AudioTrack.scss";
 
 (() => {
-    const AudioTrack = class extends HTMLElement {
-      private trackId: number;
-      private trackMessage: HTMLDivElement | null;
-      private trackDropzoneElement: HTMLDivElement | null;
-      private trackSectionList : TrackSection[];
+  const AudioTrack = class extends HTMLElement {
+    private trackId: number;
+    private trackMessage: HTMLDivElement | null;
+    private trackDropzoneElement: HTMLDivElement | null;
+    private trackSectionList: TrackSection[];
 
-      constructor() {
-        super();
-        this.trackId = 0;
-        this.trackMessage = null;
-        this.trackDropzoneElement = null;
-        this.trackSectionList = [];
-      }
-      
-      static get observedAttributes(): string[] {
-        return ['data-id'];
-      }
+    constructor() {
+      super();
+      this.trackId = 0;
+      this.trackMessage = null;
+      this.trackDropzoneElement = null;
+      this.trackSectionList = [];
+    }
 
-      attributeChangedCallback(attrName: string, oldVal: string, newVal: string): void {
-        if(oldVal !== newVal){
-          switch(attrName){
-            case 'data-id':
-              this.trackId = Number(newVal);
-              break;
-          }
-          this[attrName] = newVal;
+    static get observedAttributes(): string[] {
+      return ['data-id'];
+    }
+
+    attributeChangedCallback(attrName: string, oldVal: string, newVal: string): void {
+      if (oldVal !== newVal) {
+        switch (attrName) {
+          case 'data-id':
+            this.trackId = Number(newVal);
+            break;
         }
+        this[attrName] = newVal;
       }
+    }
 
-      connectedCallback(): void {
-        try{
-          this.render();
-          this.initElement();
-          this.initEvent();
-          this.subscribe();
-        }catch(e){
-          console.log(e); 
-        }
+    connectedCallback(): void {
+      try {
+        this.render();
+        this.initElement();
+        this.initEvent();
+        this.subscribe();
+      } catch (e) {
+        console.log(e);
       }
-  
-      render(): void {
-        this.innerHTML = `
-                    <div class="audio-track-container">
-                      <div class="audio-track-area">
+    }
+
+    render(): void {
+      this.innerHTML = `
+                    <div class="audio-track-container" event-key=${EventKeyType.FOCUS_RESET_CLICK}>
+                      <div class="audio-track-area" event-key=${EventKeyType.FOCUS_RESET_CLICK}>
                         ${this.getTrackSectionList()}
                         <div class="audio-track-massage"><span>Drag & Drop</span></div>
                         <div class="audio-track-dropzone hide" event-key=${EventKeyType.AUDIO_TRACK_DRAGOVER_DROP + this.trackId}></div>
                       </div>      
                     </div>
                 `;
+    }
+
+    getTrackSectionList(): string {
+      return this.trackSectionList.reduce((acc, trackSection, idx) =>
+        acc += `<audi-track-section data-id=${trackSection.id} data-track-id=${trackSection.trackId}></audi-track-section>`
+        , "");
+    }
+
+    initElement(): void {
+      this.trackMessage = this.querySelector('.audio-track-massage');
+      this.trackDropzoneElement = this.querySelector('.audio-track-dropzone');
+    }
+
+    initEvent(): void {
+      EventUtil.registerEventToRoot({
+        eventTypes: [EventType.dragover, EventType.drop, EventType.dragenter, EventType.dragleave],
+        eventKey: EventKeyType.AUDIO_TRACK_DRAGOVER_DROP + this.trackId,
+        listeners: [this.trackDragoverListener, this.trackDropListener, this.trackDragenterListener, this.trackDragleaveListener],
+        bindObj: this
+      });
+
+      EventUtil.registerEventToRoot({
+        eventTypes: [EventType.click],
+        eventKey: EventKeyType.FOCUS_RESET_CLICK,
+        listeners: [this.focusResetListener],
+        bindObj: this
+      });
+    }
+
+    focusResetListener(e): void {
+      const ctrlIsPressed = Controller.getCtrlIsPressed();
+      if (!ctrlIsPressed) {
+        Controller.resetFocus();
       }
+    }
 
-      getTrackSectionList(): string {
-        return this.trackSectionList.reduce((acc, trackSection, idx) => 
-          acc += `<audi-track-section data-id=${trackSection.sourceId} data-track-id=${trackSection.trackId}></audi-track-section>`, 
-        "");
+    trackDragoverListener(e): void {
+      e.preventDefault();
+      e.dataTransfer.dropEffect = "link";
+    }
+
+    trackDropListener(e): void {
+      e.preventDefault();
+      e.stopPropagation();
+      const sourceId = e.dataTransfer.getData("text/plain");
+      const source = Controller.getSourceBySourceId(Number(sourceId));
+
+      if (!source) return;
+
+      const trackSection = new TrackSection({
+        sourceId: source.id,
+        trackId: this.trackId,
+        channelStartTime: 0,
+        channelEndTime: 0,
+        parsedChannelStartTime: 0,
+        parsedChannelEndTime: 10,
+        trackStartTime: 0
+      });
+      Controller.changeTrackDragState(false);
+
+      Controller.addTrackSection(this.trackId, trackSection);
+      this.hideMessage();
+    }
+
+    trackDragenterListener(e): void {
+      e.preventDefault()
+      this.trackDropzoneElement?.classList.add('focus');
+    }
+
+    trackDragleaveListener(e): void {
+      e.preventDefault()
+      this.trackDropzoneElement?.classList.remove('focus');
+    }
+
+    hideMessage(): void {
+      this.trackMessage?.classList.add('hide');
+    }
+
+    subscribe(): void {
+      storeChannel.subscribe(StoreChannelType.TRACK_DRAG_STATE_CHANNEL, this.trackDragStateObesever, this);
+      storeChannel.subscribe(StoreChannelType.TRACK_SECTION_LIST_CHANNEL, this.trackSectionListObserver, this);
+    }
+
+    trackDragStateObesever(isTrackDraggable): void {
+      if (isTrackDraggable) {
+        this.activeTrackDropzone();
+        return;
       }
+      this.inactiveTrackDropzone();
+    }
 
-      initElement(): void {
-        this.trackMessage = this.querySelector('.audio-track-massage');
-        this.trackDropzoneElement = this.querySelector('.audio-track-dropzone');
-      }
+    activeTrackDropzone(): void {
+      this.trackDropzoneElement?.classList.remove('hide');
+    }
 
-      initEvent(): void {
-        EventUtil.registerEventToRoot({
-          eventTypes: [EventType.dragover, EventType.drop, EventType.dragenter, EventType.dragleave],
-          eventKey: EventKeyType.AUDIO_TRACK_DRAGOVER_DROP + this.trackId,
-          listeners: [this.trackDragoverListener, this.trackDropListener, this.trackDragenterListener, this.trackDragleaveListener],
-          bindObj: this
-        });
-      }
+    inactiveTrackDropzone(): void {
+      this.trackDropzoneElement?.classList.add('hide');
+    }
 
-      trackDragoverListener(e): void {
-        e.preventDefault();
-        e.dataTransfer.dropEffect = "link";
-      }
+    trackSectionListObserver({ trackId, trackSectionList }): void {
+      if (trackId !== this.trackId) return;
 
-      trackDropListener(e): void {
-        e.preventDefault();
-        e.stopPropagation();
-        const sourceId = e.dataTransfer.getData("text/plain");
-        const source = Controller.getSourceBySourceId(Number(sourceId));
-        if(!source) return;
+      this.trackSectionList = trackSectionList;
+      this.render();
+      this.initElement();
+    }
+  };
 
-        const trackSection = new TrackSection({ 
-          sourceId : source.id, 
-          trackId: this.trackId,
-          channelStartTime : 0, 
-          channelEndTime : 0, 
-          parsedChannelStartTime : 0,
-          parsedChannelEndTime: 10,
-          trackStartTime : 0
-       });
-        Controller.changeTrackDragState(false);
-        Controller.addTrackSection(this.trackId, trackSection);
-        this.hideMessage();
-      }
-      
-      trackDragenterListener(e): void {
-        e.preventDefault()
-        this.trackDropzoneElement?.classList.add('focus');
-      }
-
-      trackDragleaveListener(e): void {
-        e.preventDefault()
-        this.trackDropzoneElement?.classList.remove('focus');
-      }
-
-      hideMessage(): void {
-          this.trackMessage?.classList.add('hide');
-      }
-
-      subscribe(): void {
-        storeChannel.subscribe(StoreChannelType.TRACK_DRAG_STATE_CHANNEL, this.trackDragStateObesever, this);
-        storeChannel.subscribe(StoreChannelType.TRACK_SECTION_LIST_CHANNEL, this.trackSectionListObserver, this);
-      }
-
-      trackDragStateObesever(isTrackDraggable): void {
-        if(isTrackDraggable){
-          this.activeTrackDropzone();
-          return;
-        }
-        this.inactiveTrackDropzone();
-      }
-
-      activeTrackDropzone(): void {
-        this.trackDropzoneElement?.classList.remove('hide');
-      }
-
-      inactiveTrackDropzone(): void {
-        this.trackDropzoneElement?.classList.add('hide');
-      }
-
-      trackSectionListObserver({trackId, trackSectionList}): void {
-        if(trackId !== this.trackId) return;
-
-        this.trackSectionList = trackSectionList;
-        this.render();
-        this.initElement();
-      }
-    };
-
-    customElements.define('audi-audio-track', AudioTrack);
-  })();
+  customElements.define('audi-audio-track', AudioTrack);
+})();
--- frontend/src/components/Main/Main.ts
+++ frontend/src/components/Main/Main.ts
@@ -1,18 +1,21 @@
 import { Track } from '@model';
 import { Controller } from '@controllers';
+import { EventKeyType, EventType } from '@types'
+import { EventUtil } from '@util';
 import './Main.scss';
 
 (() => {
   const Main = class extends HTMLElement {
-    private trackList : Track[];
+    private trackList: Track[];
 
     constructor() {
       super();
-      this.trackList = Controller.getTrackList();   
+      this.trackList = Controller.getTrackList();
     }
 
     connectedCallback(): void {
       this.render();
+      this.initEvent()
     }
 
     render(): void {
@@ -22,7 +25,7 @@ import './Main.scss';
                         <aside>
                             <audi-side-bar></audi-side-bar>
                         </aside>
-                        <section class="audi-main-audio-track-container">
+                        <section class="audi-main-audio-track-container" event-key=${EventKeyType.FOCUS_RESET_CLICK}>
                             <audi-playbar></audi-playbar>
                             ${this.getTrackList()}
                         </section>
@@ -31,13 +34,29 @@ import './Main.scss';
               `;
     }
 
+    initEvent(): void {
+      EventUtil.registerEventToRoot({
+        eventTypes: [EventType.click],
+        eventKey: EventKeyType.FOCUS_RESET_CLICK,
+        listeners: [this.focusResetListener],
+        bindObj: this
+      });
+    }
+
+    focusResetListener(e): void {
+      const ctrlIsPressed = Controller.getCtrlIsPressed();
+      if (!ctrlIsPressed) {
+        Controller.resetFocus();
+      }
+    }
+
     getTrackList(): string {
-      return this.trackList.reduce((acc, cur, idx)=> 
-        acc += `<audi-audio-track data-id=${idx}></audi-audio-track>`, "");
+      return this.trackList.reduce((acc, cur, idx) =>
+        acc += `<audi-audio-track data-id=${cur.id}></audi-audio-track>`, "");
     }
   };
 
   customElements.define('audi-main', Main);
 })();
 
-export {};
+export { };
