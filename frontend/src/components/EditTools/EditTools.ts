7ae5851b4ab9477acdecb3a1f00e443b2d68d614
songwonseok <dnjstjr0517@gmail.com>
2020-12-03 11:57:04 +0900

    feat: [FE] EditTool 컴포넌트 아이콘 활성화 조건 추가
    - cursorMode, focusList, clipBoard, redo/undo list 에 따라 아이콘 버튼
      활성화
    - cursorMode는 현재 마우스가 선택하기 or 나누기 모드인지 확인
    - focusList는 현재 몇개의 구간을 선택했는 지 확인
      - 0개면 복사, 잘라내기, 삭제 모두 비활성화
      - 1개면 복사, 잘라내기, 삭제 아이콘 모두 활성화
      - 2개 이상이면 삭제 아이콘만 활성화
    - clipBoard는 현재 복사하거나 잘라내기한 객체가 clipBoard에 저장되어
      있는지 확인
      - 객체가 존재한다면 붙여넣기 아이콘 활성화
    - undoList, redoList의 길이가 1이상이면 각 undo/redo 아이콘이
      활성화됩니다.

--- frontend/src/components/EditTools/EditTools.ts
+++ frontend/src/components/EditTools/EditTools.ts
@@ -1,17 +1,39 @@
+import CommandManager from '../../command/CommandManager';
+import { CursorType, StoreChannelType } from "@types";
+import { storeChannel } from '@store';
+import { Controller } from '@controllers'
 import './EditTools.scss'
 
 (() => {
   const EditTools = class extends HTMLElement {
-    public iconlist: string[];
+    private iconlist: string[];
+    private cursorElement: HTMLElement | null;
+    private bladeElement: HTMLElement | null;
+    private copyElement: HTMLElement | null;
+    private cutElement: HTMLElement | null;
+    private pasteElement: HTMLElement | null;
+    private deleteElement: HTMLElement | null;
+    private undoElement: HTMLElement | null;
+    private redoElement: HTMLElement | null;
 
     constructor() {
       super();
       this.iconlist = ['cursor', 'blade', 'copy', 'cut', 'paste', 'delete', 'undo', 'redo'];
-
+      this.cursorElement = null;
+      this.bladeElement = null;
+      this.copyElement = null;
+      this.cutElement = null;
+      this.pasteElement = null;
+      this.deleteElement = null;
+      this.undoElement = null;
+      this.redoElement = null;
     }
 
     connectedCallback() {
       this.render();
+      this.init()
+      this.initState();
+      this.subscribe();
     }
 
     render() {
@@ -21,8 +43,77 @@ import './EditTools.scss'
               </div>
             `;
     }
+
+    init() {
+      this.cursorElement = this.querySelector('#cursor');
+      this.bladeElement = this.querySelector('#blade');
+      this.copyElement = this.querySelector('#copy');
+      this.cutElement = this.querySelector('#cut');
+      this.pasteElement = this.querySelector('#paste');
+      this.deleteElement = this.querySelector('#delete');
+      this.undoElement = this.querySelector('#undo');
+      this.redoElement = this.querySelector('#redo');
+    }
+
+    initState() {
+      this.cursotState();
+      this.focusState();
+      this.clipBoardState();
+      this.commandState();
+    }
+
+
+    cursotState() {
+      const cursorMode = Controller.getCursorMode();
+      if (cursorMode === CursorType.SELECT_MODE) {
+        this.cursorElement?.classList.add('selected');
+      } else {
+        this.bladeElement?.classList.add('selected');
+      }
+    }
+
+    focusState() {
+      const focusList = Controller.getFocusList();
+      if (focusList.length === 0) {
+        this.copyElement?.classList.add('disabled');
+        this.cutElement?.classList.add('disabled');
+        this.deleteElement?.classList.add('disabled');
+      } else if (focusList.length > 1) {
+        this.copyElement?.classList.add('disabled');
+        this.cutElement?.classList.add('disabled');
+      }
+    }
+
+    clipBoardState() {
+      const clipBoard = Controller.getClipBoard();
+      if (!clipBoard) {
+        this.pasteElement?.classList.add('disabled');
+      }
+    }
+
+
+    commandState() {
+      const { undoList, redoList } = CommandManager;
+      if (undoList.length === 0) {
+        this.undoElement?.classList.add('disabled');
+      }
+      if (redoList.length === 0) {
+        this.redoElement?.classList.add('disabled');
+      }
+    }
+
+    subscribe(): void {
+      storeChannel.subscribe(StoreChannelType.EDIT_TOOLS_CHANNEL, this.updateEditTools, this);
+    }
+
+    updateEditTools(): void {
+      this.render();
+      this.init()
+      this.initState();
+    }
+
   };
   customElements.define('audi-edit-tools', EditTools);
 })()
 
-export {};
+export { };
