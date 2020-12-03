4e180468864bf389350018dadba7aa6575f388fa
songwonseok <dnjstjr0517@gmail.com>
2020-12-03 20:42:01 +0900

    feat: [FE] 커서 모드변경 단축키 기능 추가
    - Ctrl키를 누르지 않은 상태에서 C를 누르면 나누기 모드, V를 누르면
      선택 모드로 변하도록 구현했습니다.

--- frontend/src/components/App/App.ts
+++ frontend/src/components/App/App.ts
@@ -1,5 +1,5 @@
 import './App.scss';
-import { EventDataType, eventTypes, EventTargetDataType, KeyBoard } from '@types';
+import { EventDataType, eventTypes, EventTargetDataType, KeyBoard, CursorType } from '@types';
 import { Controller } from "@controllers";
 
 (() => {
@@ -40,17 +40,16 @@ import { Controller } from "@controllers";
     KeyDownListener(e): void {
       const { } = Controller
       const isCtrl = Controller.getCtrlIsPressed();
-      console.log(e.which);
 
       if (e.which === KeyBoard.CTRL) {
         Controller.setCtrlIsPressed(true);
       }
 
       if (e.which === KeyBoard.C && !isCtrl) {
-        // console.log('Select Mode');
+        Controller.setCursorMode(CursorType.CUT_MODE);
       }
       else if (e.which === KeyBoard.V && !isCtrl) {
-        // console.log('Cut Mode');
+        Controller.setCursorMode(CursorType.SELECT_MODE);
       }
       else if (e.which === KeyBoard.DELETE && !isCtrl) {
         // console.log('삭제');
