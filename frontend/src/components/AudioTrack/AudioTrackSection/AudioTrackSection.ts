83189eb9ff039e05ba5f9fef67ee55bd4a6f3d23
Jeongeun-Choi <cje3080@gmail.com>
2020-12-04 00:56:01 +0900

    feat: [FE] 변수명 수정
    - 잘못된 변수명을 수정했습니다.

--- frontend/src/components/AudioTrack/AudioTrackSection/AudioTrackSection.ts
+++ frontend/src/components/AudioTrack/AudioTrackSection/AudioTrackSection.ts
@@ -1,36 +1,36 @@
-import { Controller } from "@controllers";
-import { EventKeyType, EventType } from '@types'
+import { Controller } from '@controllers';
+import { EventKeyType, EventType } from '@types';
 import { EventUtil } from '@util';
 
-import "./AudioTrackSection.scss";
+import './AudioTrackSection.scss';
 
-interface SectionData{
+interface SectionData {
   sectionChannelData: number[];
-  duration: number; 
+  duration: number;
 }
 
 (() => {
- const AudioTrackSection = class extends HTMLElement {
+  const AudioTrackSection = class extends HTMLElement {
     private trackId: number;
     private sectionId: number;
     private sectionData: SectionData | undefined;
     private trackCanvasElement: HTMLCanvasElement | undefined | null;
 
-    constructor(){
-        super();
-        this.trackId = 0;
-        this.sectionId = 0;
-        this.sectionData;
-        this.trackCanvasElement;
+    constructor() {
+      super();
+      this.trackId = 0;
+      this.sectionId = 0;
+      this.sectionData;
+      this.trackCanvasElement;
     }
 
     static get observedAttributes(): string[] {
       return ['data-id', 'data-track-id'];
     }
-  
+
     attributeChangedCallback(attrName: string, oldVal: string, newVal: string): void {
-      if(oldVal !== newVal){
-        switch(attrName){
+      if (oldVal !== newVal) {
+        switch (attrName) {
           case 'data-id':
             this.sectionId = Number(newVal);
             break;
@@ -41,7 +41,7 @@ interface SectionData{
         this[attrName] = newVal;
       }
     }
-        
+
     connectedCallback(): void {
       try {
         this.render();
@@ -61,38 +61,37 @@ interface SectionData{
 
     init(): void {
       this.trackCanvasElement = this.querySelector<HTMLCanvasElement>('.audio-track-section');
-      this.sectionChannelData = Controller.getSectionChannelData(this.trackId, this.sectionId);
+      this.sectionData = Controller.getSectionChannelData(this.trackId, this.sectionId);
     }
 
-    draw(): void {        
-      if(!this.sectionData || !this.trackCanvasElement) return;
+    draw(): void {
+      if (!this.sectionData || !this.trackCanvasElement) return;
 
       const { sectionChannelData, duration } = this.sectionData;
-          
+
       const trackWidth = this.trackCanvasElement.clientWidth;
-      const canvasWidth = trackWidth / (300/duration);
+      const canvasWidth = trackWidth / (300 / duration);
       this.trackCanvasElement.width = canvasWidth;
       this.trackCanvasElement.style.width = `${canvasWidth}px`;
 
       const canvasHeight = this.trackCanvasElement.clientHeight;
       const canvasCtx = this.trackCanvasElement.getContext('2d');
-      if(!canvasCtx) return;
+      if (!canvasCtx) return;
 
       const numOfPeaks = sectionChannelData.length;
       const middleHeight = canvasHeight / 2;
-      const defaultLineWidth =1;
+      const defaultLineWidth = 1;
 
       canvasCtx.strokeStyle = '#2196f3';
-      canvasCtx.lineWidth = defaultLineWidth / ((numOfPeaks/2) / canvasWidth);
+      canvasCtx.lineWidth = defaultLineWidth / (numOfPeaks / 2 / canvasWidth);
       canvasCtx.beginPath();
 
       let offsetX = 0;
       let offsetY;
-      for(let i = 0; i < numOfPeaks; i++){
-        offsetY = middleHeight + Math.floor((sectionChannelData[i]*canvasHeight)/2);
-        if(i % 2 == 0)
-          canvasCtx.moveTo(offsetX, offsetY);
-        else{
+      for (let i = 0; i < numOfPeaks; i++) {
+        offsetY = middleHeight + Math.floor((sectionChannelData[i] * canvasHeight) / 2);
+        if (i % 2 == 0) canvasCtx.moveTo(offsetX, offsetY);
+        else {
           canvasCtx.lineTo(offsetX, offsetY);
           offsetX += canvasCtx.lineWidth;
         }
@@ -113,6 +112,6 @@ interface SectionData{
       Controller.toggleFocus(this.trackId, this.sectionId, e.target);
     }
   };
-  
+
   customElements.define('audi-track-section', AudioTrackSection);
 })();
