9c352f6058530ee4cedda391c023cfbd9e232b4c
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-12-08 06:11:32 +0900

    refactor: [FE] SplitCommand 코드 리팩토링
    - excute 함수 작은 함수단위로 분리
    - PlayBarUtil은 PlayBar와 관련되어 보이는데 섹션을 나누는 커맨드에서
      사용되어 코드의 가독성이 떨어지고, PlayBarUtil은 리팩토링 과정이
      필요하므로 해당 부분을 삭제하고, 필요한 로직부분만 추가하도록 수정
      (추후 공통 유틸이 만들어지면 수정필요)
    - 트랙 섹션을 나눌 때, 파형이 이상하게 나누어지는 버그 수정.
      trackStartTime, channelStartTime 설정로직 수정하여 해결

--- frontend/src/common/command/SplitCommand.ts
+++ frontend/src/common/command/SplitCommand.ts
@@ -3,52 +3,72 @@ import { Controller } from '@controllers'
 import { StoreChannelType } from '@types'
 import { storeChannel } from '@store'
 import { Track, TrackSection } from '@model'
-import { CopyUtil, PlayBarUtil } from '@util'
+import { CopyUtil } from '@util'
 
 export class SplitCommand extends ICommand {
   private beforeTrack: Track;
   private cursorPosition: number;
-  private trackContainerElement: HTMLElement | null;
-  private targetSection: TrackSection;
+  private trackContainerElement: HTMLDivElement | null;
+  private targetTrackSection: TrackSection;
 
-  constructor(cursorPosition: number, currentTrack: Track, targetSection: TrackSection) {
+  constructor(cursorPosition: number, currentTrack: Track, targetTrackSection: TrackSection) {
     super();
     this.cursorPosition = cursorPosition;
     this.beforeTrack = currentTrack;
-    this.targetSection = targetSection;
+    this.targetTrackSection = targetTrackSection;
     this.trackContainerElement = document.querySelector('.audi-main-audio-track-container');
   }
 
-  execute() {
-    if (!this.trackContainerElement) return;
-    const startX = this.trackContainerElement.getBoundingClientRect().left;
-    const endX = this.trackContainerElement.getBoundingClientRect().right;
-    const [minute, second, milsecond, location, totalCursorTime] = PlayBarUtil.getCursorPosition(startX, this.cursorPosition, endX - startX);
+  execute(): void {
+    if(!this.trackContainerElement) return;
 
-    const splitTime = totalCursorTime - this.targetSection.trackStartTime;
-    const leftSection = CopyUtil.copySection(this.targetSection);
-    leftSection.id = 0;
-    leftSection.channelEndTime = splitTime;
-    leftSection.parsedChannelEndTime = splitTime;
-    leftSection.length = splitTime;
-
-    const rightSection = CopyUtil.copySection(this.targetSection);
-    rightSection.id = 0;
-    rightSection.channelStartTime = splitTime;
-    rightSection.parsedChannelStartTime = splitTime;
-    rightSection.trackStartTime += splitTime;
-    rightSection.length -= splitTime;
-
-    const sectionIndex = this.beforeTrack.trackSectionList.findIndex(section => section.id === this.targetSection.id);
-
-    if (sectionIndex === -1) return;
-    const trackId = this.beforeTrack.id;
-    Controller.removeSection(trackId, sectionIndex);
-    Controller.addTrackSection(trackId, leftSection);
-    Controller.addTrackSection(trackId, rightSection);
+    const timeOfCursorPosition = this.calculateTimeOfCursorPosition(this.trackContainerElement);
+    const [leftTrackSection, rightTrackSection] = this.splitTrackSection(timeOfCursorPosition);
+    this.updateSplitedSections(leftTrackSection, rightTrackSection);
   };
 
-  undo() {
+  calculateTimeOfCursorPosition(trackContainerElement: HTMLDivElement): number{
+    const trackContainerLeftX = trackContainerElement.getBoundingClientRect().left;
+    const trackContainerRightX = trackContainerElement.getBoundingClientRect().right;
+    const trackContainerWidth = trackContainerRightX - trackContainerLeftX;
+
+    const trackPixelsPerSecond = parseFloat((trackContainerWidth / 300).toFixed(2));
+    const secondsPerTrackPixel = parseFloat((1 / trackPixelsPerSecond).toFixed(2));
+    const cursorOffset = this.cursorPosition - trackContainerLeftX;
+    const timeOfCursorPosition = secondsPerTrackPixel * cursorOffset;
+
+    return timeOfCursorPosition;
+  }
+
+  splitTrackSection(timeOfCursorPosition: number): TrackSection[] {
+    const channelSplitOffset = timeOfCursorPosition - this.targetTrackSection.trackStartTime;
+    const channelSplitTime = this.targetTrackSection.channelStartTime + channelSplitOffset;
+    
+    const leftTrackSection = CopyUtil.copySection(this.targetTrackSection);
+    leftTrackSection.id = 0;
+    leftTrackSection.channelEndTime = channelSplitTime;
+    leftTrackSection.length = leftTrackSection.channelEndTime - leftTrackSection.channelStartTime;
+
+    const rightTrackSection = CopyUtil.copySection(this.targetTrackSection);
+    rightTrackSection.id = 0;
+    rightTrackSection.channelStartTime = channelSplitTime;
+    rightTrackSection.length = rightTrackSection.channelEndTime - rightTrackSection.channelStartTime;
+    rightTrackSection.trackStartTime += channelSplitOffset;
+
+    return [leftTrackSection, rightTrackSection];
+  }
+
+  updateSplitedSections(leftTrackSection: TrackSection, rightTrackSection: TrackSection): void {
+    const targetTrackSectionIndex = this.beforeTrack.trackSectionList.findIndex(trackSection => trackSection.id === this.targetTrackSection.id);
+    if (targetTrackSectionIndex === -1) return;
+    
+    const trackId = this.beforeTrack.id;
+    Controller.removeSection(trackId, targetTrackSectionIndex);
+    Controller.addTrackSection(trackId, leftTrackSection);
+    Controller.addTrackSection(trackId, rightTrackSection);
+  }
+
+  undo(): void {
     const newTrack = CopyUtil.copyTrack(this.beforeTrack);
     Controller.setTrack(newTrack);
 
