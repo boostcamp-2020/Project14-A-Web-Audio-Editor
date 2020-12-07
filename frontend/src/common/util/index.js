664e7c84a752c97a89b88118c3cfac4359d83b15
Jeongeun-Choi <cje3080@gmail.com>
2020-12-08 00:02:03 +0900

    feat: [FE] SectionEffectList와 관련된 Util 생성
    - hideEffectList는 section 선택을 하지 않았을때 EffectList를 숨기고
      SourceList를 보여주는 함수입니다.
    - showEffectList는 section 선택을 했을 때 EffectList를 보여주고
      SourceList를 숨기는 함수입니다.

--- frontend/src/common/util/index.js
+++ frontend/src/common/util/index.js
@@ -4,4 +4,5 @@ export * as AudioUtil from './AudioUtil';
 export * as PlayBarUtil from './PlayBarUtil';
 export * as MarkerEventUtil from './MarkerEventUtil';
 export * as CopyUtil from './CopyUtil';
+export * as SectionEffectListUtil from './SectionEffectListUtil';
 export { saveFile } from './compressor';
