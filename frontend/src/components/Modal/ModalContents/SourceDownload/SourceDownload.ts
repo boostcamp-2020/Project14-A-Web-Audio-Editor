6065b97198683b694a98866ab784a3175c28177d
songwonseok <dnjstjr0517@gmail.com>
2020-12-08 10:45:10 +0900

    refactor: [FE] SourceDowload saveFile 인자 변경
    - 기존에 test용으로 사용하던 audio파일을 제거하고 인자를
      compressorObject만 넘겨주도록 수정

--- frontend/src/components/Modal/ModalContents/SourceDownload/SourceDownload.ts
+++ frontend/src/components/Modal/ModalContents/SourceDownload/SourceDownload.ts
@@ -51,7 +51,7 @@ import './SourceDownload.scss';
                   </label>
                   <label>
                     <input type="radio" name="extention" value="wav" checked event-key=${EventKeyType.SOURCE_DOWNLOAD_EXTENTION_CHANGE}></input>
-                    wav (44.1kHz)
+                    wav (48kHz)
                   </label>
                 </div>
                 <div class="radios quality-radios visible-hidden">
@@ -129,11 +129,7 @@ import './SourceDownload.scss';
       this.changeBtnValue('압축 중');
       this.inactiveSaveButton(this.saveButton);
 
-      const URL = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3';
-      const response = await window.fetch(URL);
-      const arrayBuffer = await response.arrayBuffer();
-
-      await saveFile(arrayBuffer, compressorObject);
+      await saveFile(compressorObject);
       this.changeBtnValue('저장하기');
 
       this.activeSaveButton(this.saveButton);
