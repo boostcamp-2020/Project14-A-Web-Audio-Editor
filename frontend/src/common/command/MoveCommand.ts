66779d08ad742bdd503f45653d88d6efcc4ec17a
Jeongeun-Choi <cje3080@gmail.com>
2020-12-09 18:59:39 +0900

    feat: [FE] MoveCommand 생성
    - 드래그하여 trackSection을 이동할 수 있는 MoveCommand를 생성했습니다.
      - dragstart 할 때의 커서 시간에서 trackStartTime을 빼준 차이를 구합니다.
      - 그런 후 drop시의 커서 시간에서 빼준 차이를 다시 빼준게
        newTrackStartTime입니다.
      - checkEnterTrack은 아직 미구현 함수입니다!

---
+++ frontend/src/common/command/MoveCommand.ts
@@ -0,0 +1,75 @@
+import ICommand from './ICommand';
+import { Track, TrackSection } from '@model';
+import { StoreChannelType } from '@types';
+import { storeChannel } from '@store';
+import { CopyUtil } from '@util';
+import { Controller } from '@controllers';
+
+export class MoveCommand extends ICommand {
+  private prevTrack: Track;
+  private currentTrack: Track;
+  private trackSection: TrackSection;
+  private movingCursorTime: number;
+  private prevCursorTime: number;
+
+  constructor(prevTrack: Track, currentTrack: Track, trackSection: TrackSection, movingCursorTime: number, prevCursorTime: number) {
+    super();
+    this.prevTrack = prevTrack;
+    this.currentTrack = currentTrack;
+    this.trackSection = trackSection;
+    this.movingCursorTime = movingCursorTime;
+    this.prevCursorTime = prevCursorTime;
+  }
+
+  execute(): void {
+    const sectionIndex = this.prevTrack.trackSectionList.findIndex((section) => section.id === this.trackSection.id);
+
+    if (sectionIndex === -1) return;
+
+    let newTrackStartTime = this.movingCursorTime - (this.prevCursorTime - this.trackSection.trackStartTime);
+    const newTrackEndTime = newTrackStartTime + this.trackSection.length;
+
+    // if (this.checkEnterTrack(this.currentTrack.trackSectionList, newTrackStartTime, newTrackEndTime)) return;
+
+    if (newTrackStartTime < 0) {
+      newTrackStartTime = 0;
+    }
+
+    const newTrackSection = CopyUtil.copySection(this.trackSection);
+    newTrackSection.trackId = this.currentTrack.id;
+    newTrackSection.trackStartTime = newTrackStartTime;
+    newTrackSection.audioStartTime = newTrackStartTime;
+
+    Controller.removeSection(this.prevTrack.id, sectionIndex);
+    Controller.addTrackSection(this.currentTrack.id, newTrackSection);
+  }
+
+  undo(): void {
+    Controller.setTrack(this.prevTrack);
+    Controller.setTrack(this.currentTrack);
+
+    storeChannel.publish(StoreChannelType.TRACK_SECTION_LIST_CHANNEL, {
+      trackId: this.prevTrack.id,
+      trackSectionList: this.prevTrack.trackSectionList
+    });
+
+    storeChannel.publish(StoreChannelType.TRACK_SECTION_LIST_CHANNEL, {
+      trackId: this.currentTrack.id,
+      trackSectionList: this.currentTrack.trackSectionList
+    });
+
+    storeChannel.publish(StoreChannelType.TRACK_CHANNEL, this.prevTrack.trackSectionList);
+    storeChannel.publish(StoreChannelType.TRACK_CHANNEL, this.currentTrack.trackSectionList);
+  }
+
+  checkEnterTrack(trackSectioList, newTrackStartTime, newTrackEndTime): boolean {
+    const nearLeftSection = trackSectioList.some(
+      (section) => section.trackStartTime <= newTrackStartTime && section.trackStartTime + section.length >= newTrackStartTime
+    );
+    const nearRightSection = trackSectioList.some(
+      (section) => section.trackStartTime <= newTrackEndTime && section.trackStartTime + section.length >= newTrackEndTime
+    );
+
+    return nearLeftSection || nearRightSection;
+  }
+}
