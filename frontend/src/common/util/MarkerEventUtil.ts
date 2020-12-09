d5da79500ca9099b9944df84b56f2c7d3675ecfc
pieisland <pieisland@naver.com>
2020-12-09 22:14:19 +0900

    feat : [FE] 재생 중 커서로 클릭한 곳에서 바로 재생
    - 재생 중에 커서로 클릭한 마커 위치에서 다시 재생할 수 있는
      audioCursorPlay 함수 호출.
    - play 함수가 호출됨.

--- frontend/src/common/util/MarkerEventUtil.ts
+++ frontend/src/common/util/MarkerEventUtil.ts
@@ -2,6 +2,7 @@ import { getDifferenceWidth } from './WidthUtil';
 import { getSplitTime, calculateTimeOfCursorPosition } from './TimeUtil';
 import { Controller } from '@controllers';
 
+//마우스가 움직일 때 불림.
 const mousemoveMarkerListener: Function = (element: HTMLElement, defaultStartX: number, mainWidth: number) => (e: Event): void => {
   if (!element) return;
   const cursorPosition = e.pageX;
@@ -13,17 +14,22 @@ const mousemoveMarkerListener: Function = (element: HTMLElement, defaultStartX: 
   if (minute < 0 && second < 0) return;
   Controller.changeCurrentPosition(cursorWidth);
   Controller.changeCursorStringTime(minute, second, milsecond);
-  Controller.changeCursorMarkerNumberTime(cursorNumberTime);
+
+  Controller.changeCursorNumberTime(cursorNumberTime);
 };
 
+//클릭 시.
 const clickMarkerListener = (element: HTMLElement) => (e: Event): void => {
   if (!element) return;
   const [currentPosition, cursorNumberTime] = Controller.getCurrentPosition();
 
   Controller.changeMarkerPlayStringTime(cursorNumberTime);
 
+  Controller.changeMarkerNumberTime(cursorNumberTime);
+
   if (!Controller.getIsPauseState()) {
-    //play 함수 실행
+    //play만 시켜주는 게 필요함. playOrPause는 일시정지가 되므로.
+    Controller.audioCursorPlay();
   }
 
   if (currentPosition < 0) return;
