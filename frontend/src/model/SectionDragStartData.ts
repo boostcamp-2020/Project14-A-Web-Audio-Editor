75f47782c231bf4200ae1c2e0e7fab8da482ec80
songwonseok <dnjstjr0517@gmail.com>
2020-12-10 18:40:55 +0900

    feat: [FE] SectionDragStartData 생성자 파라미터 변경
    - 소스리스트에서 드래그한 경우에는 prevCursorTime이 없어서 null 타입
      추가

--- frontend/src/model/SectionDragStartData.ts
+++ frontend/src/model/SectionDragStartData.ts
@@ -2,10 +2,14 @@ import { TrackSection } from "@model"
 
 class SectionDragStartData {
   public trackSection: TrackSection;
-  public prevCursorTime: number;
+  public prevCursorTime: number | null;
   public offsetLeft: number;
 
-  constructor(trackSection: TrackSection, prevCursorTime: number, offsetLeft: number) {
+  constructor({
+    trackSection,
+    prevCursorTime = null,
+    offsetLeft,
+  }) {
     this.trackSection = trackSection;;
     this.prevCursorTime = prevCursorTime;
     this.offsetLeft = offsetLeft;
