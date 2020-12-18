bd236e56097651c75c0017dd38fdcf21b1ec7e83
songwonseok <dnjstjr0517@gmail.com>
2020-12-18 10:55:43 +0900

    fix: [FE] 줌 인/아웃 관련 마커 위치 버그 해결

--- frontend/src/components/Main/MainTrackContent/Marker/Marker.ts
+++ frontend/src/components/Main/MainTrackContent/Marker/Marker.ts
@@ -1,15 +1,20 @@
 import './Marker.scss';
 import { storeChannel } from '@store';
 import { StoreChannelType } from '@types';
+import { Controller, ZoomController } from '@controllers';
 
 (() => {
   const Marker = class extends HTMLElement {
+    private markerElement: HTMLElement | null;
     constructor() {
       super();
+      this.markerElement = null;
     }
 
     connectedCallback(): void {
       this.render();
+      this.initElement();
+      this.initMarkerPosition();
       this.subscribe();
     }
 
@@ -19,17 +24,34 @@ import { StoreChannelType } from '@types';
         `;
     }
 
-    subscribe(): void {
-      storeChannel.subscribe(StoreChannelType.CURRENT_POSITION_CHANNEL, this.updateMarkerPosition, this);
-      storeChannel.subscribe(StoreChannelType.RESET_MARKER_POSITION_CHANNEL, this.initMarkerPosition, this);
+    initElement() {
+      this.markerElement = this.querySelector('.marker');
     }
 
-    initMarkerPosition(newCurrentPosition): void {
-      const markerElement: HTMLElement | null = document.querySelector('.marker');
+    subscribe(): void {
+      storeChannel.subscribe(StoreChannelType.CURRENT_POSITION_CHANNEL, this.updateMarkerPosition, this);
+      storeChannel.subscribe(StoreChannelType.RESET_MARKER_POSITION_CHANNEL, this.changeMarkerPosition, this);
+      storeChannel.subscribe(StoreChannelType.CHANGE_TRACK_CHANNEL, this.changeTrackObserverCallback, this);
+    }
 
-      if (!markerElement) return;
+    initMarkerPosition(): void {
+      const markerTime = Controller.getMarkerTime();
+      const pixelPerSecond = ZoomController.getCurrentPixelPerSecond();
+      if (!this.markerElement) return;
 
-      markerElement.style.left = `${newCurrentPosition}px`;
+      this.markerElement.style.left = `${markerTime * pixelPerSecond}px`;
+    }
+
+    changeMarkerPosition(newCurrentPosition): void {
+      if (!this.markerElement) return;
+
+      this.markerElement.style.left = `${newCurrentPosition}px`;
+    }
+
+    changeTrackObserverCallback(): void {
+      const scrollAreaElement = document.querySelector('.audi-main-audio-track-scroll-area');
+      if (!scrollAreaElement || !this.markerElement) return;
+      this.markerElement.style.height = `${scrollAreaElement.scrollHeight}px`;
     }
 
     updateMarkerPosition(newCurrentPosition): void {
@@ -46,4 +68,4 @@ import { StoreChannelType } from '@types';
   customElements.define('audi-marker', Marker);
 })();
 
-export {};
+export { };
